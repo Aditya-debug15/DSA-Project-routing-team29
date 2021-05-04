20e326e8f8b76e1aae485a3dd9c8881083a30e93
Aditya <aadityamalhotra1512@gmail.com>
2021-05-04 18:07:22 +0530

    Created extract min which returns the minimum and delete that from the heap

--- priority_queue.c
+++ priority_queue.c
@@ -59,7 +59,7 @@ void              MinHeapify(ptr_Minheap p)
     }
 }
 
-/*
+
 ptr_node_for_heap ExtractMin(ptr_Minheap p)
 {
     if (p->size > 0)
@@ -78,7 +78,7 @@ ptr_node_for_heap ExtractMin(ptr_Minheap p)
     }
 }
 
-
+/*
 void              DecreaseKey(ptr_Minheap p,int vertex,double new_weight)
 {
     int i= p->position[vertex-1];
