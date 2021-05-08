95ecdc65076ec9a5f06a5de9c85042b17f0c7cb9
Aditya <aadityamalhotra1512@gmail.com>
2021-05-08 23:43:04 +0530

    Updated graph code

--- Adj_graph.py
+++ Adj_graph.py
@@ -6,7 +6,7 @@ for i in range(0,5):
   element=float(input())
   x.append(element)
 y=['Day 1','Day 2','Day 3','Day 4','Day 5']
-py.bar(y,x)
+py.plot(y,x)
 py.title('Tme taken to do from source to destination in last 5 days')
 py.ylabel('Time in minutes')
 py.xlabel('Days ')
