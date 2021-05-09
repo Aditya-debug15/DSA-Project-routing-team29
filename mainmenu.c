003f1d72b4b6ff41fcaa852b0e0f45b96b243094
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2021-05-09 15:19:11 +0530

    Final Main Menu
    Added two more functions. The code is self explatory but may add some comments soon.

--- mainmenu.c
+++ mainmenu.c
@@ -8,12 +8,11 @@ int main()
 {
     int choice, repeat, n, degree;
     PtrAdjList G;
-    PtrAdjList graph;
     timeHistoryTable *T;
 
-    printf("WELCOME!\n ROUTING\n");
-    printf("(Mini Project - Group 29 under TA Mr. Tanish Lad\n)");
-    printf("By Aditya Malhotra(2020101052)\tParshva Bhadra(2020101001)\tAnubhav Pal(2020112012)\tKarnati Jahnavi(2020102052)\tAaryan Sharma(2020115008)");
+    printf("WELCOME TO ROUTING!\n");
+    printf("Mini Project of Group 29 under TA Mr. Tanish Lad\n");
+    printf("\nBy\nAditya Malhotra(2020101052)\nParshva Bhadra(2020101001)\nAnubhav Pal(2020112012)\nKarnati Jahnavi(2020102052)\nAaryan Sharma(2020115008)\n\n");
 
     printf("Please enter the total number of vertices: \n");
     scanf("%d", &degree);
@@ -22,20 +21,22 @@ int main()
     T = createEmptyTimeHistoryTable(degree);
 
 MENU:
-    printf("\n");
-    printf("To add an edge, press 1\n");
-    printf("To delete an edge, press 2\n");
-    printf("To update distance, press 3\n");
-    printf("To update number of cars, press 4\n");
-    printf("To find shortest distance, press 5\n");
-    printf("To find least time, press 6\n");
-    printf("To find a safe path, press 7\n");
-    printf("To exit the program, press 8\n\n");
+    printf("\nMenu:\n");
+    printf("To add an edge, Press 1\n");
+    printf("To delete an edge, Press 2\n");
+    printf("To update distance, Press 3\n");
+    printf("To update number of cars, Press 4\n");
+    printf("To find shortest distance, Press 5\n");
+    printf("To find least time, Press 6\n");
+    printf("To find a safe path, Press 7\n");
+    printf("To print the graph, Press 8\n");
+    printf("To print the time history table, Press 9\n");
+    printf("To exit the program, Press 10\n\n");
     scanf("%d", &choice);
 
-    if (choice < 1 || choice > 9)
+    if (choice < 1 || choice >= 11)
     {
-        printf("\nInvalid command!\n");
+        printf("\nInvalid Command! Please Choose Again.\n");
         goto MENU;
     }
 
@@ -47,6 +48,7 @@ MENU:
 
     POINT1:
         scanf("%d", &n);
+       
         if (n == 1)
         {
             int vertex1, vertex2, cars;
@@ -61,7 +63,7 @@ MENU:
             scanf("%d", &cars);
             InsertEdge(G, vertex1, vertex2, length, cars);
 
-            printf("Successfully added an edge. \n");
+            printf("\nSuccessfully added an edge. \n");
             printf("Do you want to add another edge?\n");
             printf("If YES, Press 1 \nIf No, Press 0\n");
 
@@ -77,7 +79,7 @@ MENU:
             }
             if (repeat != 1 || repeat != 0)
             {
-                printf("Invalid Command! Please Enter again.\n");
+                printf("Invalid Command! Please Enter Again.\n");
                 goto POINT12;
             }
         }
@@ -89,7 +91,7 @@ MENU:
 
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter Again.\n");
             goto POINT1;
         }
     }
@@ -97,21 +99,22 @@ MENU:
     if (choice == 2)
     {
 
-        printf("\nYou have selected to delete an edge\n");
+        printf("\nYou have selected to delete an edge.\n");
         printf("To continue, Press 1\nTo go back to the main menu, Press 0\n");
 
     POINT2:
         scanf("%d", &n);
+       
         if (n == 1)
         {
             int vertex1, vertex2;
 
         POINT21:
-            printf("Enter the vertices between which you want to delete the edge");
+            printf("Enter the vertices between which you want to delete the edge:\n");
             scanf("%d%d", &vertex1, &vertex2);
             DeleteEdge(G, vertex1, vertex2);
 
-            printf("Successfully deleted the desired edge. \n");
+            printf("\nSuccessfully deleted the desired edge.\n");
             printf("Do you want to delete another edge?\n");
             printf("If YES, Press 1 \nIf No, Press 0\n");
 
@@ -127,7 +130,7 @@ MENU:
             }
             if (repeat != 1 || repeat != 0)
             {
-                printf("Invalid Command! Please Enter again.\n");
+                printf("Invalid Command! Please Enter Again.\n");
                 goto POINT22;
             }
         }
@@ -139,32 +142,33 @@ MENU:
 
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter Again.\n");
             goto POINT2;
         }
     }
 
     if (choice == 3)
     {
-        printf("You have selected to update distance \n");
+        printf("You have selected to update distance.\n");
         printf("To continue, Press 1\nTo go back to the main menu, Press 0\n");
 
     POINT3:
 
         scanf("%d", &n);
+       
         if (n == 1)
         {
             int vertex1, vertex2;
             double new_length;
 
         POINT31:
-            printf("Enter the vertices between which you want to update distance\n");
+            printf("Enter the vertices between which you want to update distance:\n");
             scanf("%d%d", &vertex1, &vertex2);
-            printf("Enter the new distance\n");
+            printf("Enter the new distance:\n");
             scanf("%lf", &new_length);
             UpdateDis(G, vertex1, vertex2, new_length);
 
-            printf("Successfully updated the distance. \n");
+            printf("\nSuccessfully updated the distance. \n");
             printf("Do you want to update another distance?\n");
             printf("If YES, Press 1 \nIf No, Press 0\n");
 
@@ -180,7 +184,7 @@ MENU:
             }
             if (repeat != 1 || repeat != 0)
             {
-                printf("Invalid Command! Please Enter again.\n");
+                printf("Invalid Command! Please Enter Again.\n");
                 goto POINT32;
             }
         }
@@ -192,31 +196,32 @@ MENU:
 
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command. Please Enter Again.\n");
             goto POINT3;
         }
     }
 
     if (choice == 4)
     {
-        printf("You have selected to update number of cars \n");
+        printf("You have selected to update number of cars. \n");
         printf("To continue, Press 1\nTo go back to the main menu, Press 0\n");
 
     POINT4:
         scanf("%d", &n);
+       
         if (n == 1)
         {
             int vertex1, vertex2;
             int new_cars;
 
         POINT41:
-            printf("Enter the vertices between which you want to update number of cars\n");
+            printf("Enter the vertices between which you want to update number of cars:\n");
             scanf("%d%d", &vertex1, &vertex2);
-            printf("Enter the updated number of cars\n");
+            printf("Enter the updated number of cars:\n");
             scanf("%d", &new_cars);
             UpdateCars(G, vertex1, vertex2, new_cars);
 
-            printf("Successfully updated the number of cars. \n");
+            printf("\nSuccessfully updated the number of cars. \n");
             printf("Do you want to update another number of cars?\n");
             printf("If YES, Press 1 \nIf No, Press 0\n");
 
@@ -232,29 +237,31 @@ MENU:
             }
             if (repeat != 1 || repeat != 0)
             {
-                printf("Invalid Command! Please Enter again.\n");
+                printf("Invalid Command! Please Enter Again.\n");
                 goto POINT42;
             }
         }
+
         if (n == 0)
         {
             goto MENU;
         }
+
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter Again.\n");
             goto POINT4;
         }
     }
 
     if (choice == 5)
     {
-
-        printf("You have selected to find shortest distance \n");
-        printf("To continue, Press 1,\nTo go back to the main menu, Press 0\n");
+        printf("You have selected to find shortest distance.\n");
+        printf("To continue, Press 1\nTo go back to the main menu, Press 0\n");
 
     POINT5:
         scanf("%d", &n);
+       
         if (n == 1)
         {
             int source, destination;
@@ -264,9 +271,9 @@ MENU:
             scanf("%d", &source);
             printf("Enter the destination of the path: \n");
             scanf("%d", &destination);
-            shortest_distance_choice4(graph, source, destination);
+            shortest_distance_choice4(G, source, destination);
 
-            printf("Successfully found the shortest distance. \n");
+            printf("\nSuccessfully found the shortest distance. \n");
             printf("Do you want to find shortest distance between another source and destination?\n");
             printf("If YES, Press 1 \nIf No, Press 0\n");
 
@@ -282,7 +289,7 @@ MENU:
             }
             if (repeat != 1 || repeat != 0)
             {
-                printf("Invalid Command! Please Enter again.\n");
+                printf("Invalid Command! Please Enter Again.\n");
                 goto POINT52;
             }
         }
@@ -294,7 +301,7 @@ MENU:
 
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter Again.\n");
             goto POINT5;
         }
     }
@@ -302,22 +309,24 @@ MENU:
     if (choice == 6)
     {
 
-        printf("You have selected to find least time \n");
-        printf("To continue, Press 1,\nTo go back to the main menu, Press 0\n");
+        printf("You have selected to find least time. \n");
+        printf("To continue, Press 1\nTo go back to the main menu, Press 0\n");
 
     POINT6:
         scanf("%d", &n);
+       
         if (n == 1)
         {
             int source, destination;
 
         POINT61:
-            printf("Please enter the source of path");
+            printf("Please enter the source of path:\n");
             scanf("%d", &source);
-            printf("Please enter the destination of the path");
+            printf("Please enter the destination of the path:\n");
             scanf("%d", &destination);
-            shortest_time_choice5(graph, source, destination);
-            printf("Successfully found the least time! \n");
+            shortest_time_choice5(G, source, destination);
+
+            printf("\nSuccessfully found the least time!\n");
             printf("Do you want to find least time between another source and destination?\n");
             printf("If YES, Press 1 \nIf No, Press 0\n");
 
@@ -333,7 +342,7 @@ MENU:
             }
             if (repeat != 1 || repeat != 0)
             {
-                printf("Invalid Command! Please Enter again.\n");
+                printf("Invalid Command! Please Enter Again.\n");
                 goto POINT62;
             }
         }
@@ -345,7 +354,7 @@ MENU:
 
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter Again.\n");
             goto POINT6;
         }
     }
@@ -353,19 +362,20 @@ MENU:
     if (choice == 7)
     {
 
-        printf("You have selected to find a safe distance \n");
-        printf("To continue, Press 1,\nTo go back to the main menu, Press 0\n");
+        printf("You have selected to find a safe distance.\n");
+        printf("To continue, Press 1\nTo go back to the main menu, Press 0\n");
 
     POINT7:
         scanf("%d", &n);
+        
         if (n == 1)
         {
             int source, destination;
 
         POINT71:
-            printf("Please enter the source of path");
+            printf("Please enter the source of path: \n");
             scanf("%d", &source);
-            printf("Please enter the destination of the path");
+            printf("Please enter the destination of the path:\n");
             scanf("%d", &destination);
             SafeAlgorithmStart(T, G, source, destination);
             printf("Successfully found the safest path. \n");
@@ -396,35 +406,126 @@ MENU:
 
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter again.\n");
             goto POINT7;
         }
     }
 
     if (choice == 8)
     {
-
-        printf("You have selected to exit the program \n");
-        printf("To continue, Press 1,\nTo stay in the main menu, Press 0\n");
-
+        printf("You have selected to print the graph.\n");
+        printf("To continue, Press 1\n To go back to the main menu , Press 0\n");
     POINT8:
         scanf("%d", &n);
         if (n == 1)
         {
-            DeleteList(G);
-            //parshav T//
-            printf("Thank You, Have a nice day");
+        POINT81:
+            PrintList(G);
+            printf("\nSuccessfully printed the graph.\n");
+            printf("Do you want to print the graph again?\n");
+            printf("If YES, Press 1 \nIf No, Press 0\n");
+
+        POINT82:
+            scanf("%d", &repeat);
+            if (repeat == 1)
+            {
+                goto POINT81;
+            }
+            if (repeat == 0)
+            {
+                goto MENU;
+            }
+            if (repeat != 1 || repeat != 0)
+            {
+                printf("Invalid Command! Please Enter Again.\n");
+                goto POINT82;
+            }
         }
+
         if (n == 0)
         {
             goto MENU;
         }
+
         if (n != 0 || n != 1)
         {
-            printf("Invalid Command. Please Enter again.\n");
+            printf("Invalid Command! Please Enter Again.\n");
             goto POINT8;
         }
     }
 
+    if (choice == 9)
+    {
+        printf("You have selected to print the time history table.\n");
+        printf("To continue, Press 1\n To go back to the main menu , Press 0\n");
+    POINT9:
+        scanf("%d", &n);
+
+        if (n == 1)
+        {
+        POINT91:
+            printTimeHistoryTable(T);
+            printf("\nSuccessfully printed the time history table. \n");
+            printf("Do you want to print the time history table again?\n");
+            printf("If YES, Press 1 \nIf No, Press 0\n");
+
+        POINT92:
+            scanf("%d", &repeat);
+            if (repeat == 1)
+            {
+                goto POINT91;
+            }
+            if (repeat == 0)
+            {
+                goto MENU;
+            }
+            if (repeat != 1 || repeat != 0)
+            {
+                printf("Invalid Command! Please Enter Again.\n");
+                goto POINT92;
+            }
+        }
+
+        if (n == 0)
+        {
+            goto MENU;
+        }
+
+        if (n != 0 || n != 1)
+        {
+            printf("Invalid Command! Please Enter Again.\n");
+            goto POINT9;
+        }
+    }
+
+    if (choice == 10)
+    {
+
+        printf("You have selected to exit the program. \n");
+        printf("To continue, Press 1\nTo stay in the main menu, Press 0\n");
+
+    POINT10:
+        scanf("%d", &n);
+
+        if (n == 1)
+        {
+            DeleteList(G);
+            deleteMemoryOfTable(T);
+            printf("Thank You, Have a nice day!\n");
+            goto EXIT;
+        }
+
+        if (n == 0)
+        {
+            goto MENU;
+        }
+
+        if (n != 0 || n != 1)
+        {
+            printf("Invalid Command! Please Enter Again.\n");
+            goto POINT10;
+        }
+    }
+EXIT:
     return 0;
 }
